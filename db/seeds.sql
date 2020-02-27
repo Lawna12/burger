d30dc5a8fd0a0219de51e84fe6013ea252524053
AndrewL <lawna12@gmail.com>
2020-02-26 19:37:55 -0500

    seeds.sql added

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Big Hurt", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Got Me Seeing Double", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Night Night", true);
